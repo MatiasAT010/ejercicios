a37356bdb6f576c895e968c26e08479f292334ef
MatiasAT010 <matiasagudelo5a@gmail.com>
2025-04-12 13:41:55 -0500

    Update and rename README.md to Figuras.java

---
+++ Figuras.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class Figuras {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("A que figura quieres sacarle el area?");
+         System.out.println("1. Al circulo");
+         System.out.println("2. Al triangulo");
+         System.out.println("3. Al rectangulo");
+         String opcion = sc.nextLine();
+         double resultado=0;
+         
+         switch(opcion){
+         case "1":
+             System.out.println("Ingresa el radio del circulo: ");
+             double radioC = sc.nextDouble();
+             resultado = radioC * radioC * 3.1416;
+             System.out.println("El area de tu circulo es: " + resultado);
+         case "2":
+             System.out.println("Ingresa la base de tu triangulo: ");
+             double base = sc.nextDouble();
+             System.out.println("Ingresa la altura de tu triangulo: ");
+             double altura = sc.nextDouble();
+             resultado = base * altura / 2;
+             System.out.println("El area de tu triangulo es: " + resultado);
+         case "3":
+             System.out.println("Ingresa el ancho de tu rectangulo: ");
+             double ancho = sc.nextDouble();
+             System.out.println("Ingresa el largo de tu rectangulo: ");
+             double largo = sc.nextDouble();
+             resultado = largo * ancho;
+             System.out.println("El area de tu rectangulo es: " + resultado);
+             
+         }
+    }
+}
